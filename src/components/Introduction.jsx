1ca464612ab47bc79ed6f497ed18afe567fe8b59
dylon <dmychel@tutanota.com>
2024-07-15 00:01:26 -0400

    created component

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,21 @@
+export default function Introduction() {
+  return (
+    <div className="intro">
+      <div className="text">
+        <p>
+          I’m a Designer with a focus on developing user friendly interfaces.
+          <br />
+          <br />
+          I also have a strong background in Art and Technology, giving me a
+          well a balanced approach to design.
+          <br />
+          <br />I approach designing user interfaces with user friendliness as
+          the top priority.
+        </p>
+      </div>
+      <div className="logo">
+        <img src="/assets/icons/white.png" alt="" />
+      </div>
+    </div>
+  );
+}
