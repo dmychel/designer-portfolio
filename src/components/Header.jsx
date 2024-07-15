22440362587f14c825c3b7e8f01468ec2391f223
dylon <dmychel@tutanota.com>
2024-07-15 00:00:53 -0400

    created comp

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="logo">
+        <img src="/assets/icons/white.png" alt="" />
+      </div>
+      <nav>
+        <ul>
+          <li>HOME</li>
+          <li>WORK</li>
+          <li>ABOUT</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
