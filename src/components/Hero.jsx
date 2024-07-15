dd7faead5fe6bfd9a010a41fbeaad5af605627fc
dylon <dmychel@tutanota.com>
2024-07-15 00:01:39 -0400

    created component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+  return (
+    <div className="hero">
+      <div className="text">
+        <h1>Designer</h1>
+        <p>Dylon Crowley-Perez</p>
+      </div>
+    </div>
+  );
+}
