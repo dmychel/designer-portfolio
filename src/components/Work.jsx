12d6cb22ed6285682bcda6dbe8521f5a9b4b642e
dylon <dmychel@tutanota.com>
2024-07-16 19:03:05 -0400

    edited object properties

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,25 +2,25 @@ export default function Work() {
   const data = [
     {
       id: crypto.randomUUID(),
-      image: "/assets/icons/crucial-white.png",
+      image: "https://placehold.co/600x400",
       title: "Crucial Clothing",
       info: "Desgined a E-commerce website for a clothing brand startup.",
     },
     {
       id: crypto.randomUUID(),
-      image: "/assets/icons/crucial-white.png",
+      image: "https://placehold.co/600x400",
       title: "Ephemeral Photography",
       info: "Designed a Photography Portfolio",
     },
     {
       id: crypto.randomUUID(),
-      image: "/assets/icons/crucial-white.png",
+      image: "https://placehold.co/600x400",
       title: "BWH Video Archive",
       info: "Built and designed a video internal archive website for training videos.",
     },
     {
       id: crypto.randomUUID(),
-      image: "/assets/icons/crucial-white.png",
+      image: "https://placehold.co/600x400",
       title: "Nehemiah Yisrael Graphic Design",
       info: "Designed a portfolio website for a freelancer.",
     },
