d2e28db5495a98e24ed7ecefa7ae616e79e24e62
dmychel <dmychel@tutanota.com>
2024-07-17 11:56:05 -0400

    changed jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,25 +2,25 @@ export default function Work() {
   const data = [
     {
       id: crypto.randomUUID(),
-      image: "https://placehold.co/600x400",
+      image: "https://placehold.co/600x450",
       title: "Crucial Clothing",
       info: "Desgined a E-commerce website for a clothing brand startup.",
     },
     {
       id: crypto.randomUUID(),
-      image: "https://placehold.co/600x400",
+      image: "https://placehold.co/600x450",
       title: "Ephemeral Photography",
       info: "Designed a Photography Portfolio",
     },
     {
       id: crypto.randomUUID(),
-      image: "https://placehold.co/600x400",
+      image: "https://placehold.co/600x450",
       title: "BWH Video Archive",
       info: "Built and designed a video internal archive website for training videos.",
     },
     {
       id: crypto.randomUUID(),
-      image: "https://placehold.co/600x400",
+      image: "https://placehold.co/600x450",
       title: "Nehemiah Yisrael Graphic Design",
       info: "Designed a portfolio website for a freelancer.",
     },
@@ -33,11 +33,9 @@ export default function Work() {
             <img src={project.image} alt="" />
           </div>
           <div className="card_info">
-            <div className="text">
-              <h2>{project.title}</h2>
-              <p>{project.info}</p>
-              <button>View Case Study</button>
-            </div>
+            <h2>{project.title}</h2>
+            <p>{project.info}</p>
+            <button>View Case Study</button>
           </div>
         </div>
       ))}
