d5995eaaa8ef33a4bf45ce697cc2170ef5f7f91e
dmychel <dmychel@tutanota.com>
2024-07-17 19:41:57 -0400

    moved router logic to app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "/src/App.jsx";
-import About from "/src/routes/About.jsx";
-import Error from "/src/routes/Error.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "*",
-    element: <Error />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
