862b836c0ed392881844436a6826361c4307b1e4
dmychel <dmychel@tutanota.com>
2024-07-17 19:42:45 -0400

    created jsx

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,3 +1,23 @@
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <div className="about">
+      <div className="text">
+        <p>
+          My professional life has certainly bounced around. However there is a
+          common theme amongst the jobs that have left an impact on me, and that
+          theme is creation. Whether it be technically driven or art driven I
+          have a passion for creating. I was born and raised in the low-income
+          parts of Boston Massachusetts. Being in this environment forced me to
+          grow up fast, but also gave me the opportunity to become involved in a
+          lot of communities that would shape who I am today. Outside of work I
+          try to maintain as many hobbies as possible. I’ve been dancing since I
+          was a kid and still do today. I enjoy photography, rock climbing,
+          tricking (acrobatic discipline), and a bit of no Gi Jiu-Jitsu.
+        </p>
+      </div>
+      <div className="photo">
+        <img src="/assets/images/profile-photo.png" alt="" />
+      </div>
+    </div>
+  );
 }
