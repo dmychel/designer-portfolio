c6519c9ebde02ab14724bfdae9845190441499d3
dmychel <dmychel@tutanota.com>
2024-07-17 20:01:23 -0400

    jsx update

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -6,13 +6,19 @@ export default function About() {
           My professional life has certainly bounced around. However there is a
           common theme amongst the jobs that have left an impact on me, and that
           theme is creation. Whether it be technically driven or art driven I
-          have a passion for creating. I was born and raised in the low-income
-          parts of Boston Massachusetts. Being in this environment forced me to
-          grow up fast, but also gave me the opportunity to become involved in a
-          lot of communities that would shape who I am today. Outside of work I
-          try to maintain as many hobbies as possible. I’ve been dancing since I
-          was a kid and still do today. I enjoy photography, rock climbing,
-          tricking (acrobatic discipline), and a bit of no Gi Jiu-Jitsu.
+          have a passion for creating.
+          <br />
+          <br />
+          I was born and raised in the low-income parts of Boston Massachusetts.
+          Being in this environment forced me to grow up fast, but also gave me
+          the opportunity to become involved in a lot of communities that would
+          shape who I am today.
+          <br />
+          <br />
+          Outside of work I try to maintain as many hobbies as possible. I’ve
+          been dancing since I was a kid and still do today. I enjoy
+          photography, rock climbing, tricking (acrobatic discipline), and a bit
+          of no Gi Jiu-Jitsu.
         </p>
       </div>
       <div className="photo">
